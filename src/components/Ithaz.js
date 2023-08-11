2d3f9228e18f4282ad69bbc5fbd6cc6e2bac7834
Mohanraj293 <mohanrajrengarajr@gmail.com>
2023-08-11 17:07:11 +0530

    Minor update

--- src/components/Ithaz.js
+++ src/components/Ithaz.js
@@ -1,16 +1,21 @@
 import React from "react";
+import "../css/Ithaz.css";
 
-const Book = ({ title, year, pdfUrl, index }) => {
+const Book = ({ title, year, pdfUrl, image }) => {
   const handleOpenPDF = () => {
     window.open(pdfUrl, "_blank");
   };
 
   return (
-    <tr onClick={handleOpenPDF}>
-      <td>{index}</td>
-      <td>{title}</td>
-      <td>{year}</td>
-    </tr>
+    <div className="ithaz">
+      <img className="image" src={image} alt="" width={150} height={200} />
+      <p
+        style={{ textDecoration: "underline", cursor: "pointer" }}
+        onClick={handleOpenPDF}
+      >
+        {title}
+      </p>
+    </div>
   );
 };
 
--- src/components/Ithazgal.js
+++ src/components/Ithazgal.js
@@ -1,42 +1,43 @@
 import React from "react";
 import { MDBTable, MDBTableHead, MDBTableBody } from "mdb-react-ui-kit";
+import "../css/Ithazgal.css";
 import Ithaz from "./Ithaz";
 import AprilPdf from "../assets/ithazgal-pdf/APRIL-JUNE-ISSUE-2023.pdf";
+import JanMarchPdf from "../assets/ithazgal-pdf/JAN-MARCH ISSUE-2023.pdf";
+import JanMarchImage from "../assets/ithazgal-pdf/JAN-MARCH-2023.jpg";
+import AprilImage from "../assets/ithazgal-pdf/APRIL-JUNE-2023.jpg";
 
 const Ithazgal = ({ title }) => {
   document.title = title;
 
   const books = [
     {
-      title: "APRIL-JUNE-ISSUE-2023",
-      label: "Author 1",
-      year: 2020,
+      title: "JAN MARCH ISSUE 2023",
+      year: 2023,
+      pdfUrl: JanMarchPdf,
+      label: JanMarchImage,
+    },
+    {
+      title: "APRIL JUNE ISSUE 2023",
+      year: 2023,
       pdfUrl: AprilPdf,
+      label: AprilImage,
     },
   ];
 
   return (
     <div style={{ padding: "10px" }}>
       <h1 style={{ textAlign: "center" }}>இதழ்கள்</h1>
-      <MDBTable bordered responsive>
-        <MDBTableHead light>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Book Name</th>
-            <th scope="col">Published Year</th>
-          </tr>
-        </MDBTableHead>
-        <MDBTableBody>
-          {books.map((book, index) => (
-            <Ithaz
-              index={index + 1}
-              title={book.title}
-              year={book.year}
-              pdfUrl={book.pdfUrl}
-            />
-          ))}
-        </MDBTableBody>
-      </MDBTable>
+      <div>
+        {books.map((book) => (
+          <Ithaz
+            image={book.label}
+            title={book.title}
+            year={book.year}
+            pdfUrl={book.pdfUrl}
+          />
+        ))}
+      </div>
     </div>
   );
 };
